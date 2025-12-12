5d3eac4aaa07bdeee3227f7557fb206714cee9f1
Grandschtroumpf <francois.guezengar@hotmail.fr>
2025-12-12 11:17:24 +0100

    support mobile

--- src/routes/simulator-v6/index.tsx
+++ src/routes/simulator-v6/index.tsx
@@ -1,4 +1,4 @@
-import { component$, unwrapStore, useComputed$, useStore, useStyles$, useVisibleTask$ } from "@builder.io/qwik";
+import { $, component$, unwrapStore, useComputed$, useStore, useStyles$, useVisibleTask$ } from "@builder.io/qwik";
 import { useSignal } from "@builder.io/qwik";
 import { transition$ } from "~/transition";
 import { Option, steps } from './steps';
@@ -89,19 +89,19 @@ export default component$(() => {
     }
   });
 
-  const edit = transition$((simulation: Simulation) => {
+  const edit = $((simulation: Simulation) => {
     answers.splice(0, Infinity, ...simulation);
     current.value = simulation.at(-1)!.question;
   });
 
-  const remove = transition$((index: number) => {
+  const remove = $((index: number) => {
     simulations.splice(index, 1);
   });
 
   return (
     <main>
       <aside>
-        <ul>
+        <ul class="simulations">
           {simulations.map((simulation, i) => (
             <li key={i}>
               <span>{getSimulationName(simulation)}</span>
@@ -120,9 +120,6 @@ export default component$(() => {
         </ul>
       </aside>
       <section>
-        <header>
-          <h1>Estimation: <span class="price">{price}</span></h1>
-        </header>
         <ol>
             {answers.map((answer, index) => (
               <>
@@ -171,8 +168,35 @@ export default component$(() => {
               </menu>
             </li>
           </ol>
+        <footer>
+          <button class="menu" commandfor="simulations-dialog" command="show-modal">
+            <svg height="24px" viewBox="0 -960 960 960" width="24px" fill="black">
+              <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/>
+            </svg>
+          </button>
+          <h1>Estimation: <span class="price">{price}</span></h1>
+        </footer>
       </section>
       <a id="mailto" hidden href=""></a>
+      <dialog id="simulations-dialog" closedby="any">
+        <ul class="simulations">
+          {simulations.map((simulation, i) => (
+            <li key={i}>
+              <span>{getSimulationName(simulation)}</span>
+              <button onClick$={() => edit(simulation)}>
+                <svg height="24px" viewBox="0 -960 960 960" width="24px" stroke="black">
+                  <path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/>
+                </svg>
+              </button>
+              <button onClick$={() => remove(i)}>
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="black">
+                  <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/>
+                </svg>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </dialog>
     </main>
   )
 })
